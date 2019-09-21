648035ff51b0a918569c65ff5bf00da18d07eff8
amadeusj5 <amadeusjosue5@gmail.com>
2019-09-20 18:45:32 -0600

    Agregar paginas de error 403, 404 y 505

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Page Not Found</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body
+      class="d-flex align-items-center border-top border-top-2 border-primary">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-5 col-xl-4 my-5">
+                <div class="text-center">
+                    <!-- Preheading -->
+                    <h6 class="text-uppercase text-muted mb-4">
+                        404 error
+                    </h6>
+
+                    <!-- Heading -->
+                    <h1 class="display-4 mb-3">
+                        There’s no page here 😭
+                    </h1>
+
+                    <!-- Subheading -->
+                    <p class="text-muted mb-4">
+                        Looks like you ended up here by accident?
+                    </p>
+
+                    <!-- Button -->
+                    <a href="{{ route('admin.dashboard') }}"
+                       class="btn btn-lg btn-primary">
+                        Return to your dashboard
+                    </a>
+                </div>
+            </div>
+        </div> <!-- / .row -->
+    </div> <!-- / .container -->
+</body>
+
+</html>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Internal Error</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body
+      class="d-flex align-items-center border-top border-top-2 border-primary">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-5 col-xl-4 my-5">
+                <div class="text-center">
+                    <!-- Preheading -->
+                    <h6 class="text-uppercase text-muted mb-4">
+                        500 error
+                    </h6>
+
+                    <!-- Heading -->
+                    <h1 class="display-4 mb-3">
+                        There’s no page here 😭
+                    </h1>
+
+                    <!-- Subheading -->
+                    <p class="text-muted mb-4">
+                        Looks like something went wrong
+                    </p>
+
+                    <!-- Button -->
+                    <a href="{{ route('admin.dashboard') }}"
+                       class="btn btn-lg btn-primary">
+                        Return to your dashboard
+                    </a>
+                </div>
+            </div>
+        </div> <!-- / .row -->
+    </div> <!-- / .container -->
+</body>
+
+</html>
