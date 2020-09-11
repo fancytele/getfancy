156de8224ff52298aeb25c03662a3f9fe64eef33
kulpreet <kulpreet@i22.in>
2020-09-11 18:57:39 +0530

    Otp_changes_email_changes

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -41,6 +41,6 @@ class WelcomeMail extends Mailable
      */
     public function build()
     {
-        return $this->view('mails.welcome')->subject("Welcome to Fancy");
+        return $this->view('mails.welcome')->subject("Welcome to Fancyy");
     }
 }
