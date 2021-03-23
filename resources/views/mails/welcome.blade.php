7fe0acc9293baba15a80398e728c4b856b1cb49a
Siddhant Jain <37961299+Siddhantj929@users.noreply.github.com>
2021-03-23 20:14:13 +0530

    Removed favicon

--- resources/views/mails/welcome.blade.php
+++ resources/views/mails/welcome.blade.php
@@ -5,7 +5,6 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="icon" href="favicon.png" type="image/png">
 </head>
 
 <body
